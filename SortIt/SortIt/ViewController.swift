22656ba92acc80aaf44f1c1951b66cdf33759cff
Laurens <laurensb75@gmail.com>
2020-06-11 16:58:02 +0700

    initial commit

---
+++ SortIt/SortIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SortIt
+//
+//  Created by Laurens Bryan Cahyana on 11/06/20.
+//  Copyright © 2020 laurens bryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
